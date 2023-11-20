e991c5310dda00f311c8475bc65ddac82b0659e7
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-11-20 15:42:31 +0600

    image cropped done

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let inputFile = document.getElementById("input-file");
 let profileImg = document.getElementById("profile-img");
+let profileImgEmpty = document.getElementById("profile-img-empty");
 
 inputFile.onchange = function () {
-    profileImg.src = URL.createObjectURL(inputFile.files[0])
+    profileImgEmpty.src = URL.createObjectURL(inputFile.files[0])
 }
+
+// cropping image
+const cropper = new Cropper(profileImg, {
+    aspectRatio: 0,
+    viewMode: 0,
+})
+
+// action after click
+document.getElementById("cropped-img-btn").addEventListener("click", () => {
+    let croppedImg = cropper.getCroppedCanvas().toDataURL("image/png");
+    
+    document.getElementById("cropped-output-img").src = croppedImg;
+})
